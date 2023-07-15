c5bed3b68181d55ba02c842bc206c202b29ec15c
tob-asco <tobias.hochreiter@gmx.at>
2023-07-15 21:46:08 +0200

    add DalBaseService and make UIIService its child

--- seeds.Dal/Services/UserIdeaInteractionService.cs
+++ seeds.Dal/Services/UserIdeaInteractionService.cs
@@ -5,56 +5,27 @@ using System.Net.Http.Json;
 
 namespace seeds.Dal.Services;
 
-public class UserIdeaInteractionService : IUserIdeaInteractionService
+public class UserIdeaInteractionService : DalBaseService, IUserIdeaInteractionService
 {
-    private readonly IHttpClientWrapper _httpClientWrapper;
     public UserIdeaInteractionService(IHttpClientWrapper httpClientWrapper)
-    {
-        _httpClientWrapper = httpClientWrapper;
-    }
+        : base(httpClientWrapper) { }
     public async Task<UserIdeaInteraction?> GetUserIdeaInteractionAsync(
         string username, int ideaId)
     {
-        try
-        {
-            var response = await _httpClientWrapper.GetAsync(
-                $"api/UserIdeaInteractions/{username}/{ideaId}");
-            response.EnsureSuccessStatusCode(); // important, otherwise it will use std. model
-            return await response.Content.ReadFromJsonAsync<UserIdeaInteraction>();
-        }
-        catch (Exception ex)
-        {
-            // All types of exceptions will land here, e.g.
-            // timeout, no such user, server overload, ...
-            // not sure if this is expected behaviour. (TODO)
-            //return await Task.FromException<UserIdeaInteraction>(ex)
-            //    .ConfigureAwait(false);
-            Console.Write(ex.Message);
-            return null;
-        }
+        string url = $"api/UserIdeaInteractions/{username}/{ideaId}";
+        return await GetDalModelAsync<UserIdeaInteraction>(url);
     }
 
     public async Task<bool> PutUserIdeaInteractionAsync(string username, int ideaId, bool newUpvoted, bool newDownvoted)
     {
-        try
+        string url = $"api/UserIdeaInteractions/{username}/{ideaId}";
+        UserIdeaInteraction newUii = new()
         {
-            var httpContent = JsonContent.Create(new UserIdeaInteraction
-            {
-                Username = username,
-                IdeaId = ideaId,
-                Downvoted = newDownvoted,
-                Upvoted = newUpvoted
-            });
-            var response = await _httpClientWrapper.PutAsync(
-                $"api/UserIdeaInteractions/{username}/{ideaId}",
-                httpContent);
-            response.EnsureSuccessStatusCode();
-            return true;
-        }
-        catch (Exception ex)
-        {
-            Console.Write(ex.Message);
-            return false;
-        }
+            Username = username,
+            IdeaId = ideaId,
+            Downvoted = newDownvoted,
+            Upvoted = newUpvoted
+        };
+        return await PutDalModelAsync<UserIdeaInteraction>(url, newUii);
     }
 }
