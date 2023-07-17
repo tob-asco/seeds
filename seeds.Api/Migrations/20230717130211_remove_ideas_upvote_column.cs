e888150d118ceede9b72a6cc64a7e50c885630c4
tob-asco <tobias.hochreiter@gmx.at>
2023-07-17 15:03:05 +0200

    Migrated to DB

---
+++ seeds.Api/Migrations/20230717130211_remove_ideas_upvote_column.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace seeds.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_ideas_upvote_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "upvotes",
+                table: "ideas");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "upvotes",
+                table: "ideas",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
