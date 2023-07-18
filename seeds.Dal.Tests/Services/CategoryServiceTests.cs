41adab2e8c063c42f2c8050c62e1b56f440e735e
tob-asco <tobias.hochreiter@gmx.at>
2023-07-18 11:20:33 +0200

    CategoryServiceTests: Get: pos. & neg.

--- seeds.Dal.Tests/Services/CategoryServiceTests.cs
+++ seeds.Dal.Tests/Services/CategoryServiceTests.cs
@@ -1,69 +1,46 @@
-﻿using seeds.Dal.Model;
+﻿using seeds.Dal.Interfaces;
+using seeds.Dal.Model;
 using seeds.Dal.Services;
 using seeds.Dal.Wrappers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 
 namespace seeds.Dal.Tests.Services;
 
 public class CategoryServiceTests
 {
-    private readonly IHttpClientWrapper _httpClientWrapper;
-    private readonly CategoryService _service;
+    private readonly IDalBaseService _baseService;
+    private readonly ICategoryService _service;
     public CategoryServiceTests()
     {
-        _httpClientWrapper = A.Fake<IHttpClientWrapper>();
-        _service = new CategoryService(_httpClientWrapper);
+        _baseService = A.Fake<IDalBaseService>();
+        _service = A.Fake<ICategoryService>();
     }
 
     [Fact]
     public async Task CatService_GetCatByKeyAsync_ReturnsCategory()
     {
-        #region Arrange
+        // Arrange
         string key = "ABC";
-        var cat = new Category()
-        {
-            Key = key,
-            Name = "ABeCe"
-        };
-        var response = new HttpResponseMessage
-        {
-            StatusCode = HttpStatusCode.OK,
-            Content = new StringContent(JsonSerializer.Serialize(cat)),
-        };
-        A.CallTo(() => _httpClientWrapper.GetAsync(A<string>.Ignored))
-            .Returns(response);
-        #endregion
+        var cat = new Category() { Key = key, Name = "ABeCe" };
+        A.CallTo(() => _baseService.GetDalModelAsync<Category>(A<string>.Ignored))
+            .Returns(cat);
 
         // Act
         var result = await _service.GetCategoryByKeyAsync(key);
 
         // Assert
-        result.Should().BeEquivalentTo(cat);
+        result.Should().NotBeNull();
+        result?.Key.Should().Be(key);
     }
     [Fact]
     public async Task CatService_GetCatByKeyAsync_IfNotExistReturnsNull()
     {
-        #region Arrange
-        string key = "ABC";
-        var cat = new Category()
-        {
-            Key = key,
-            Name = "ABeCe"
-        };
-        var response = new HttpResponseMessage
-        {
-            StatusCode = HttpStatusCode.NotFound,
-        };
-        A.CallTo(() => _httpClientWrapper.GetAsync(A<string>.Ignored))
-            .Returns(response);
-        #endregion
+        // Arrange
+        A.CallTo(() => _baseService.GetDalModelAsync<Category>(A<string>.Ignored))
+            .Returns(null);
 
         // Act
-        var result = await _service.GetCategoryByKeyAsync(key);
+        var result = await _service.GetCategoryByKeyAsync("N0C");
 
         // Assert
         result.Should().BeNull();
