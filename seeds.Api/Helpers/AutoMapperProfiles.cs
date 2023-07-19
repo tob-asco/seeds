fc5353d79578f5ab0e4ba2be772aed78a0469c90
tob-asco <tobias.hochreiter@gmx.at>
2023-07-19 14:50:57 +0200

    add IdeaDtoApi model and AutoMapperProfiles (setting up AM in the Api and Api.Tests is in some earlier commit...)

---
+++ seeds.Api/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using seeds.Dal.Dto.ToApi;
+using seeds.Dal.Model;
+
+namespace seeds.Api.Helpers;
+
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        CreateMap<IdeaDtoApi, Idea>();
+    }
+}
---
+++ seeds.Dal/Dto/ToApi/IdeaDtoApi.cs
@@ -0,0 +1,15 @@
+﻿namespace seeds.Dal.Dto.ToApi;
+
+public class IdeaDtoApi
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "Idea's Short Title";
+    public string Slogan { get; set; } = "Idea's Short Slogan";
+    public string CreatorName { get; set; } = "tobi";
+    //public DateTime CreationTime { get; set; } = DateTime.Now;
+    public string CategoryKey { get; set; } = "NoC";
+    public string Slide1 { get; set; } = "https://images.twinkl.co.uk/tr/raw/upload/u/ux/lightbulb-1875247-1920_ver_1.jpg";
+    public string Slide2 { get; set; } = "";
+    public string Slide3 { get; set; } = "";
+
+}
