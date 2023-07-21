246a3379bf2d127070bda09a3e1b949711b50f1d
tob-asco <tobias.hochreiter@gmx.at>
2023-07-21 17:27:36 +0200

    implement INavigationService in MockNavigationService

--- seeds1.Tests/Services/MockNavigationService.cs
+++ seeds1.Tests/Services/MockNavigationService.cs
@@ -1,16 +1,12 @@
 ﻿using seeds1.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace seeds1.Tests.Services;
 
 public class MockNavigationService : INavigationService
 {
     public bool NavigationCalled { get; private set; } = false;
+    public bool RedrawNavigationTarget { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
     public Task NavigateToAsync(string url, IDictionary<string, object> navParameters)
     {
         NavigationCalled = true;
