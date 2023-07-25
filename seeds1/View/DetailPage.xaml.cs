fd574945729d0f2be3c666d87619812f0cac3fbb
tob-asco <tobias.hochreiter@gmx.at>
2023-07-25 15:35:08 +0200

    Presentation: add LoadAndDisplayPresentationCommand and call it OnNavigatedTo

--- seeds1/View/DetailPage.xaml.cs
+++ seeds1/View/DetailPage.xaml.cs
@@ -2,9 +2,18 @@ namespace seeds1.View;
 
 public partial class DetailPage : ContentPage
 {
+	private readonly DetailViewModel vm;
 	public DetailPage(DetailViewModel vm)
 	{
 		InitializeComponent();
+		this.vm = vm;
 		BindingContext = vm;
 	}
+
+    protected async override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+
+		await vm.LoadAndDisplayPresentation();
+    }
 }
--- seeds1/ViewModel/DetailViewModel.cs
+++ seeds1/ViewModel/DetailViewModel.cs
@@ -1,4 +1,7 @@
-﻿using seeds1.Interfaces;
+﻿using Android.App;
+using seeds.Dal.Interfaces;
+using seeds.Dal.Model;
+using seeds1.Interfaces;
 using seeds1.MauiModels;
 
 namespace seeds1.ViewModel;
@@ -6,11 +9,41 @@ namespace seeds1.ViewModel;
 [QueryProperty(nameof(FeedEntry), nameof(FeedEntry))]
 public partial class DetailViewModel : MyBaseViewModel
 {
+    private readonly IPresentationService presentationService;
+
     public FeedEntry FeedEntry { get; set; }
     public DetailViewModel(
-        IGlobalService globalService)
+        IGlobalService globalService,
+        IPresentationService presentationService)
         : base(globalService)
     {
+        this.presentationService = presentationService;
+    }
 
+    [ObservableProperty]
+    string displayedDescription = "";
+
+    [RelayCommand]
+    public async Task LoadAndDisplayPresentation()
+    {
+        /* Call OnNavigatedTo
+         */
+        DisplayedDescription = "";
+        seeds.Dal.Model.Presentation presi;
+        try
+        {
+            presi = await presentationService.GetPresentationByIdeaIdAsync(FeedEntry.Idea.Id);
+        }
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlert("DB Error", ex.Message, "Ok");
+            return;
+        }
+        if (presi == null)
+        {
+            DisplayedDescription = "No description provided.";
+            return;
+        }
+        DisplayedDescription = presi.Description;
     }
 }
