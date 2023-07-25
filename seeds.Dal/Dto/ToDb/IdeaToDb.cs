563a2ab19c0d4351d7fdea7019614f852065eb69
tob-asco <tobias.hochreiter@gmx.at>
2023-07-25 19:52:58 +0200

    add IdeaToDb

---
+++ seeds.Dal/Dto/ToDb/IdeaToDb.cs
@@ -0,0 +1,12 @@
+﻿namespace seeds.Dal.Dto.ToDb;
+
+public class IdeaToDb
+{
+    public string Title { get; set; } = "Idea's Short Title";
+    public string Slogan { get; set; } = "Idea's Short Slogan";
+    public string CreatorName { get; set; } = "tobi";
+    public string CategoryKey { get; set; } = "NoC";
+    public string Slide1 { get; set; } = "https://images.twinkl.co.uk/tr/raw/upload/u/ux/lightbulb-1875247-1920_ver_1.jpg";
+    public string Slide2 { get; set; } = "";
+    public string Slide3 { get; set; } = "";
+}
