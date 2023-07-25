8d5eff0452db59f8227446342f2a77c2ab40ddf8
tob-asco <tobias.hochreiter@gmx.at>
2023-07-25 11:44:05 +0200

    Presentation: created EF Core model

---
+++ seeds.Dal/Model/Presentation.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace seeds.Dal.Model;
+
+/* No navigation properties, so also no DTO for this one.
+ */
+
+[Table("presentation")]
+public class Presentation
+{
+    [Column("id")]
+    [Key] //Primary Key
+    public int Id { get; set; }
+
+    [Column("idea_id")]
+    public int IdeaId { get; set; }
+
+    [Column("description")]
+    public string Description { get; set; } = "";
+}
