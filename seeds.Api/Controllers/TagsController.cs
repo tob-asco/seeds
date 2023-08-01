53674f99e330dcb2aa8e44e1fce35d8cfade060a
tob-asco <tobias.hochreiter@gmx.at>
2023-08-01 16:19:11 +0200

    scaffold out TagsController.cs

---
+++ seeds.Api/Controllers/TagsController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using seeds.Api.Data;
+using seeds.Dal.Model;
+
+namespace seeds.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+        private readonly seedsApiContext _context;
+
+        public TagsController(seedsApiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tags
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tag>>> GetTag()
+        {
+          if (_context.Tag == null)
+          {
+              return NotFound();
+          }
+            return await _context.Tag.ToListAsync();
+        }
+
+        // GET: api/Tags/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tag>> GetTag(string id)
+        {
+          if (_context.Tag == null)
+          {
+              return NotFound();
+          }
+            var tag = await _context.Tag.FindAsync(id);
+
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            return tag;
+        }
+
+        // PUT: api/Tags/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTag(string id, Tag tag)
+        {
+            if (id != tag.CategoryKey)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tag).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TagExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tags
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Tag>> PostTag(Tag tag)
+        {
+          if (_context.Tag == null)
+          {
+              return Problem("Entity set 'seedsApiContext.Tag'  is null.");
+          }
+            _context.Tag.Add(tag);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TagExists(tag.CategoryKey))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTag", new { id = tag.CategoryKey }, tag);
+        }
+
+        // DELETE: api/Tags/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTag(string id)
+        {
+            if (_context.Tag == null)
+            {
+                return NotFound();
+            }
+            var tag = await _context.Tag.FindAsync(id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tag.Remove(tag);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TagExists(string id)
+        {
+            return (_context.Tag?.Any(e => e.CategoryKey == id)).GetValueOrDefault();
+        }
+    }
+}
