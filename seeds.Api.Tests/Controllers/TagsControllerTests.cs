930c86959686b8517f9e3ae247f128bec05acbe1
tob-asco <tobias.hochreiter@gmx.at>
2023-08-01 16:41:17 +0200

    improve TagsController and test it

---
+++ seeds.Api.Tests/Controllers/TagsControllerTests.cs
@@ -0,0 +1,103 @@
+﻿using seeds.Dal.Dto.ToAndFromDb;
+using seeds.Dal.Model;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace seeds.Api.Tests.Controllers;
+
+public class TagsControllerTests : ApiBaseControllerTests
+{
+    public List<Category> Categories { get; set; } = new();
+    public List<Tag> Tags { get; set; } = new();
+
+    public TagsControllerTests()
+    {
+        PopulatePropertiesAndAddToDb();
+        _context.SaveChanges();
+        // Clear the change tracker, so each test has a fresh _context
+        _context.ChangeTracker.Clear();
+    }
+    private void PopulatePropertiesAndAddToDb()
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            Categories.Add(
+            new Category()
+            {
+                Key = $"Cat{i}",
+            });
+        }
+        if(!_context.Category.Any()) { _context.Category.AddRange(Categories); }
+        for (int i = 0; i <= 29; i++)
+        {
+            Tags.Add(
+            new Tag()
+            {
+                CategoryKey = $"Cat{(int)i/10}",
+                Name = $"tag{i}"
+            });
+        }
+        if(!_context.Tag.Any()) { _context.Tag.AddRange(Tags); }
+    }
+
+    [Fact]
+    public async Task TagsController_GetAllEndpoint_ReturnsListOfCorrectLength()
+    {
+        // Arrange
+        string url = $"api/Tags";
+
+        // Act
+        var response = await _httpClient.GetAsync(url);
+        var result = await response.Content.ReadFromJsonAsync<List<TagDto>>();
+
+        // Assert
+        response.Should().BeSuccessful();
+        result.Should().NotBeNull();
+        result?.Should().HaveCount(Tags.Count);
+    }
+    [Fact]
+    public async Task TagsController_GetAllEndpoint_IfEmptyReturnsNotFound()
+    {
+        // Arrange
+        _context.Tag.RemoveRange(Tags);
+        _context.SaveChanges();
+        string url = $"api/Tags";
+
+        // Act
+        var response = await _httpClient.GetAsync(url);
+
+        // Assert
+        response.Should().HaveStatusCode(HttpStatusCode.NotFound);
+    }
+    [Fact]
+    public async Task TagsController_GetEndpoint_ReturnsTag()
+    {
+        // Arrange
+        string catKey = Categories[0].Key;
+        string name = Tags[0].Name;
+        string url = $"api/Tags/{catKey}/{name}";
+
+        // Act
+        var response = await _httpClient.GetAsync(url);
+        var result = await response.Content.ReadFromJsonAsync<TagDto>();
+
+        // Assert
+        response.Should().BeSuccessful();
+        result.Should().NotBeNull();
+        result?.CategoryKey.Should().Be(catKey);
+        result?.Name.Should().Be(name);
+    }
+    [Fact]
+    public async Task TagsController_GetEndpoint_IfNotExistReturnsNotFound()
+    {
+        // Arrange
+        string url = $"api/Tags/ThisShouldBeNoValidKey/ok";
+
+        // Act
+        var response = await _httpClient.GetAsync(url);
+
+        // Assert
+        response.Should().HaveStatusCode(HttpStatusCode.NotFound);
+    }
+
+}
--- seeds.Api/Controllers/TagsController.cs
+++ seeds.Api/Controllers/TagsController.cs
@@ -1,134 +1,50 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using seeds.Api.Data;
+using seeds.Dal.Dto.ToAndFromDb;
 using seeds.Dal.Model;
 
-namespace seeds.Api.Controllers
+namespace seeds.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TagsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TagsController : ControllerBase
+    private readonly seedsApiContext context;
+    private readonly IMapper mapper;
+
+    public TagsController(
+        seedsApiContext context,
+        IMapper mapper)
     {
-        private readonly seedsApiContext _context;
+        this.context = context;
+        this.mapper = mapper;
+    }
 
-        public TagsController(seedsApiContext context)
-        {
-            _context = context;
-        }
+    // GET: api/Tags
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TagDto>>> GetTags()
+    {
+        if (context.Tag == null) { return NotFound(); }
 
-        // GET: api/Tags
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Tag>>> GetTag()
-        {
-          if (_context.Tag == null)
-          {
-              return NotFound();
-          }
-            return await _context.Tag.ToListAsync();
-        }
+        var tags = await context.Tag.ToListAsync();
+        if (tags == null || tags?.Count == 0) { return NotFound(); }
 
-        // GET: api/Tags/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Tag>> GetTag(string id)
-        {
-          if (_context.Tag == null)
-          {
-              return NotFound();
-          }
-            var tag = await _context.Tag.FindAsync(id);
+        var tagDtoList = mapper.Map<List<TagDto>>(tags);
+        return tagDtoList;
+    }
 
-            if (tag == null)
-            {
-                return NotFound();
-            }
+    // GET: api/Tags/Noc/tag
+    [HttpGet("{catKey}/{name}")]
+    public async Task<ActionResult<TagDto>> GetTag(string catKey, string name)
+    {
+        if (context.Tag == null) { return NotFound(); }
 
-            return tag;
-        }
+        var tag = await context.Tag.FindAsync(catKey, name);
+        if (tag == null) { return NotFound(); }
 
-        // PUT: api/Tags/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTag(string id, Tag tag)
-        {
-            if (id != tag.CategoryKey)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(tag).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TagExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Tags
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Tag>> PostTag(Tag tag)
-        {
-          if (_context.Tag == null)
-          {
-              return Problem("Entity set 'seedsApiContext.Tag'  is null.");
-          }
-            _context.Tag.Add(tag);
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (TagExists(tag.CategoryKey))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return CreatedAtAction("GetTag", new { id = tag.CategoryKey }, tag);
-        }
-
-        // DELETE: api/Tags/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTag(string id)
-        {
-            if (_context.Tag == null)
-            {
-                return NotFound();
-            }
-            var tag = await _context.Tag.FindAsync(id);
-            if (tag == null)
-            {
-                return NotFound();
-            }
-
-            _context.Tag.Remove(tag);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool TagExists(string id)
-        {
-            return (_context.Tag?.Any(e => e.CategoryKey == id)).GetValueOrDefault();
-        }
+        var tagDto = mapper.Map<TagDto>(tag);
+        return tagDto;
     }
 }
