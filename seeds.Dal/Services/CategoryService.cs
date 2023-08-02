4dbd33837678909b6c22ba22b2ff42389e4fc6dd
tob-asco <tobias.hochreiter@gmx.at>
2023-08-02 10:21:16 +0200

    tested GetCategoriesAsync

--- seeds.Dal/Services/CategoryService.cs
+++ seeds.Dal/Services/CategoryService.cs
@@ -4,22 +4,22 @@ namespace seeds.Dal.Services;
 
 public class CategoryService : ICategoryService 
 {
-    private readonly IDalBaseService _baseService;
+    private readonly IDalBaseService baseService;
     public CategoryService(IDalBaseService baseService)
     {
-        _baseService = baseService;
+        this.baseService = baseService;
     }
     public async Task<List<CategoryDto>> GetCategoriesAsync()
     {
         string url = $"api/Categories";
-        var baseResult = await _baseService.GetDalModelAsync<List<CategoryDto>>(url)
+        var baseResult = await baseService.GetDalModelAsync<List<CategoryDto>>(url)
             ?? throw new Exception($"The Get URL {url} returned null.");
         return baseResult;
     }
     public async Task<CategoryDto> GetCategoryByKeyAsync(string categoryKey)
     {
         string url = $"api/Categories/{categoryKey}";
-        var baseResult = await _baseService.GetDalModelAsync<CategoryDto>(url)
+        var baseResult = await baseService.GetDalModelAsync<CategoryDto>(url)
             ?? throw new Exception($"The Get URL {url} returned null.");
         return baseResult;
     }
