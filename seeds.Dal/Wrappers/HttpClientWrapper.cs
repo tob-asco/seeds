5a501f039945875fa679c686848d1999e1412ae6
tob-asco <tobias.hochreiter@gmx.at>
2023-08-10 11:10:09 +0200

    encoding url: implemented GPT function to encode URL parameters in my HttpWrapper

--- seeds.Dal/Wrappers/HttpClientWrapper.cs
+++ seeds.Dal/Wrappers/HttpClientWrapper.cs
@@ -1,4 +1,6 @@
-﻿namespace seeds.Dal.Wrappers;
+﻿using System.Web;
+
+namespace seeds.Dal.Wrappers;
 
 public class HttpClientWrapper : IHttpClientWrapper
 {
@@ -15,26 +17,52 @@ public class HttpClientWrapper : IHttpClientWrapper
         {
             //BaseAddress = new Uri("https://z4bppc68-5282.uks1.devtunnels.ms/") // #1, not working
             //BaseAddress = new Uri("https://q73sqz83-5282.euw.devtunnels.ms/") // #2
-            BaseAddress = new Uri("https://0cfsqc33-5282.euw.devtunnels.ms/") // test
+            BaseAddress = new Uri("https://0cfsqc33-5282.euw.devtunnels.ms/") // test, working
         };
         //_httpClient.BaseAddress = new Uri("http://localhost:5282/");
     }
-    public async Task<HttpResponseMessage> GetAsync(string url)
+    public string BuildFullUrl(
+        string endpointUrl, params (string Key, string Value)[] queryParams)
     {
-        return await httpClient.GetAsync(url);
+        if (string.IsNullOrWhiteSpace(endpointUrl))
+        {
+            throw new ArgumentException(
+                "Base URL cannot be empty or null.", nameof(endpointUrl));
+        }
+
+        if (queryParams == null || queryParams.Length == 0)
+        {
+            return endpointUrl;
+        }
+
+        var encodedParams = string.Join("&", queryParams
+            .Select(param => $"{param.Key}={HttpUtility.UrlEncode(param.Value)}"));
+
+        return $"{endpointUrl}?{encodedParams}";
     }
 
-    public async Task<HttpResponseMessage> PutAsync(string url, HttpContent httpContent)
+    public async Task<HttpResponseMessage> GetAsync(
+        string endpointUrl, params (string Key, string Value)[] queryParams)
     {
-        return await httpClient.PutAsync(url, httpContent);
-    }
-    public async Task<HttpResponseMessage> PostAsync(string url, HttpContent httpContent)
-    {
-        return await httpClient.PostAsync(url, httpContent);
+        return await httpClient.GetAsync(BuildFullUrl(endpointUrl, queryParams));
     }
 
-    public async Task<HttpResponseMessage> DeleteAsync(string url)
+    public async Task<HttpResponseMessage> PutAsync(
+        HttpContent httpContent,
+        string endpointUrl, params (string Key, string Value)[] queryParams)
     {
-        return await httpClient.DeleteAsync(url);
+        return await httpClient.PutAsync(BuildFullUrl(endpointUrl, queryParams), httpContent);
+    }
+    public async Task<HttpResponseMessage> PostAsync(
+        HttpContent httpContent,
+        string endpointUrl, params (string Key, string Value)[] queryParams)
+    {
+        return await httpClient.PostAsync(BuildFullUrl(endpointUrl, queryParams), httpContent);
+    }
+
+    public async Task<HttpResponseMessage> DeleteAsync(
+        string endpointUrl, params (string Key, string Value)[] queryParams)
+    {
+        return await httpClient.DeleteAsync(BuildFullUrl(endpointUrl, queryParams));
     }
 }
--- seeds.Dal/Wrappers/IHttpClientWrapper.cs
+++ seeds.Dal/Wrappers/IHttpClientWrapper.cs
@@ -1,10 +1,20 @@
-﻿namespace seeds.Dal.Wrappers;
+﻿using System.Web;
+
+namespace seeds.Dal.Wrappers;
 
 public interface IHttpClientWrapper
 {
     public Uri BaseAddress { get; set; }
-    public Task<HttpResponseMessage> GetAsync(string url);
-    public Task<HttpResponseMessage> PutAsync(string url, HttpContent httpContent);
-    public Task<HttpResponseMessage> PostAsync(string url, HttpContent httpContent);
-    public Task<HttpResponseMessage> DeleteAsync(string url);
+    public string BuildFullUrl(
+        string endpointUrl, params (string Key, string Value)[] queryParams);
+    public Task<HttpResponseMessage> GetAsync(
+        string endpointUrl, params (string Key, string Value)[] queryParams);
+    public Task<HttpResponseMessage> PutAsync(
+        HttpContent httpContent,
+        string endpointUrl, params (string Key, string Value)[] queryParams);
+    public Task<HttpResponseMessage> PostAsync(
+        HttpContent httpContent,
+        string endpointUrl, params (string Key, string Value)[] queryParams);
+    public Task<HttpResponseMessage> DeleteAsync(
+        string endpointUrl, params (string Key, string Value)[] queryParams);
 }
