78ab781bbdfb8e3b46b9a049c9c42eb5ab812342
tob-asco <tobias.hochreiter@gmx.at>
2023-08-12 19:02:57 +0200

    consumed this function in FeedEntriesServiceadded CatagPreferencesService.GetTagPreferencesOfIdea

--- seeds1/Interfaces/ICatagPreferencesService.cs
+++ seeds1/Interfaces/ICatagPreferencesService.cs
@@ -1,9 +1,15 @@
-﻿using seeds1.MauiModels;
+﻿using seeds.Dal.Dto.FromDb;
+using seeds1.MauiModels;
 
 namespace seeds1.Interfaces;
 
 public interface ICatagPreferencesService
 {
+    /// <summary>
+    /// Prepares CUP data from CurrentUser of the tags of a specified idea.
+    /// </summary>
+    /// <returns>A short list of CatagPreferences</returns>
+    public Task<List<CatagPreference>> GetTagPreferencesOfIdeaAsync(IdeaFromDb idea);
     /// <summary>
     /// Prepares CUP data from CurrentUser of categories and tags
     /// for usage in the VMs.
