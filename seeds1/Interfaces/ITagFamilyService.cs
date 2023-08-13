2d99739a3f28b816fef33dcaf558f962e6e25b2a
tob-asco <tobias.hochreiter@gmx.at>
2023-08-13 21:19:10 +0200

    add (I)TagFamilyService and injected them

---
+++ seeds1/Interfaces/ITagFamilyService.cs
@@ -0,0 +1,9 @@
+﻿using seeds1.MauiModels;
+
+namespace seeds1.Interfaces;
+
+public interface ITagFamilyService
+{
+    public List<FamilyOrPreference> ConvertToFamilyOrPreferences(List<CatagPreference> catagPrefs);
+    public Task<List<FamilyOrPreference>> GetAllFamilyOrPreferenceAsync();
+}
---
+++ seeds1/Services/TagFamilyService.cs
@@ -0,0 +1,81 @@
+﻿using seeds1.Interfaces;
+using seeds1.MauiModels;
+
+namespace seeds1.Services;
+
+public class TagFamilyService : ITagFamilyService
+{
+    private readonly ICatagPreferencesService catagPrefService;
+
+    public TagFamilyService(
+        ICatagPreferencesService catagPrefService)
+    {
+        this.catagPrefService = catagPrefService;
+    }
+    public List<FamilyOrPreference> ConvertToFamilyOrPreferences(List<CatagPreference> catagPrefs)
+    {
+        List<FamilyOrPreference> fops = new();
+        List<TagFamily> fams = new();
+        foreach (var catagPref in catagPrefs)
+        {
+            if (catagPref.TagName != null && // otherwise this is for a category
+                catagPref.TagName.Contains('|') && // the separator symbolising a family
+                catagPref.Preference == 0)
+            {
+                TagFamily fam = fams.FirstOrDefault(f => f.Name == catagPref.TagName.Split('|')[0].Trim());
+                if (fam != null)
+                {
+                    // update fams by updating fam
+                    fam.Tags.Add(new()
+                    {
+                        Name = catagPref.TagName,
+                        CategoryKey = catagPref.CategoryKey,
+                    });
+                }
+                else
+                {
+                    // add new fam and this tag to it
+                    fams.Add(new()
+                    {
+                        Name = catagPref.TagName.Split('|')[0].Trim(),
+                        Tags = new() { new()
+                        {
+                            Name = catagPref.TagName,
+                            CategoryKey = catagPref.CategoryKey,
+                        }}
+                    });
+                }
+            }
+            else
+            {
+                // this catagPref is no family member
+                fops.Add(new()
+                {
+                    IsFamily = false,
+                    Preference = new()
+                    {
+                        CategoryKey = catagPref.CategoryKey,
+                        CategoryName = catagPref.CategoryName,
+                        TagName = catagPref.TagName,
+                        Preference = catagPref.Preference,
+                    }
+                });
+            }
+        }
+        foreach (var fam in fams)
+        {
+            fops.Add(new()
+            {
+                IsFamily = true,
+                Family = fam
+            });
+        }
+        return fops;
+    }
+
+    public async Task<List<FamilyOrPreference>> GetAllFamilyOrPreferenceAsync()
+    {
+        var catagPrefs = await catagPrefService.GetCatagPreferencesAsync();
+        return ConvertToFamilyOrPreferences(catagPrefs);
+    }
+}
