1650cab1053a358a01d7fc891433f1d11873e693
tob-asco <tobias.hochreiter@gmx.at>
2023-08-13 17:39:52 +0200

    added MauiModels: TagFamily, FamilyOrPreference

---
+++ seeds1/MauiModels/FamilyOrPreference.cs
@@ -0,0 +1,12 @@
+﻿namespace seeds1.MauiModels;
+
+public partial class FamilyOrPreference : ObservableObject
+{
+    [ObservableProperty]
+    public bool isFamily = false;
+    [ObservableProperty]
+    public TagFamily family = new();
+    [ObservableProperty]
+    public CatagPreference preference = new();
+
+}
---
+++ seeds1/MauiModels/TagFamily.cs
@@ -0,0 +1,9 @@
+﻿using seeds.Dal.Dto.FromDb;
+
+namespace seeds1.MauiModels;
+
+public class TagFamily
+{
+    public string Name { get; set; }
+    public List<TagFromDb> Tags { get; set; }
+}
