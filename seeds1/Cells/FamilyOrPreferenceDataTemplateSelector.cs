c6b6a655472dd3f88b6ecd9ce52adcdc8dcce399
tob-asco <tobias.hochreiter@gmx.at>
2023-08-14 10:00:24 +0200

    add FamilyOrGroupDataTemplateSelector

---
+++ seeds1/Cells/FamilyOrPreferenceDataTemplateSelector.cs
@@ -0,0 +1,14 @@
+﻿using seeds1.MauiModels;
+
+namespace seeds1.Cells;
+
+public class FamilyOrPreferenceDataTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate FamilyTemplate { get; set; }
+    public DataTemplate PreferenceTemplate { get; set; }
+    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    {
+        var famOrPref = (FamilyOrPreference)item;
+        return famOrPref.IsFamily ? FamilyTemplate : PreferenceTemplate;
+    }
+}
