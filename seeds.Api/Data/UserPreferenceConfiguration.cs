0a6c623c5d88e9b0a0797b15db204e6a9c606261
tob-asco <tobias.hochreiter@gmx.at>
2023-08-15 19:18:55 +0200

    unstable: simplified UserPreference. removed properties {CategoryKey, TagName, Id} and gave it a Guid ItemId. Now I need to change the controllers to translate the Guids to these removed properties.

--- seeds.Api/Data/UserPreferenceConfiguration.cs
+++ seeds.Api/Data/UserPreferenceConfiguration.cs
@@ -10,15 +10,6 @@ public class UserPreferenceConfiguration : IEntityTypeConfiguration<UserPreferen
 {
     public void Configure(EntityTypeBuilder<UserPreference> builder)
     {
-        #region Constraints
-        builder.HasIndex(cup => new { cup.CategoryKey, cup.Username, cup.TagName })
-            .IsUnique();
-        #endregion
-        builder.Property(cup => cup.Id)
-            .ValueGeneratedOnAdd();
-
-        // TagName is null if the CUP is for a category
-        builder.Property(cup => cup.TagName)
-            .IsRequired(false);
+        builder.HasKey(cup => new { cup.ItemId, cup.Username });
     }
 }
