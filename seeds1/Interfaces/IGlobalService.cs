f1629a0767c593989b6c473c5b5b389c3343936f
tob-asco <tobias.hochreiter@gmx.at>
2023-08-16 15:00:19 +0200

    added a global GetPreferencesAsync (for CurrentUser)

--- seeds1/Interfaces/IGlobalService.cs
+++ seeds1/Interfaces/IGlobalService.cs
@@ -1,8 +1,15 @@
 ﻿using seeds.Dal.Dto.ToAndFromDb;
+using seeds.Dal.Model;
 
 namespace seeds1.Interfaces;
 
 public interface IGlobalService
 {
     public UserDto CurrentUser { get; set; }
+
+    /// <summary>
+    /// Gets all UserPrefernces of the CurrentUser
+    /// </summary>
+    /// <returns>A dictionary with the key given by the ItemId</returns>
+    public Task<Dictionary<Guid, UserPreference>> GetPreferencesAsync();
 }
--- seeds1/Services/GlobalService.cs
+++ seeds1/Services/GlobalService.cs
@@ -1,9 +1,35 @@
 ﻿using seeds.Dal.Dto.ToAndFromDb;
+using seeds.Dal.Interfaces;
+using seeds.Dal.Model;
 using seeds1.Interfaces;
 
 namespace seeds1.Services;
 
 public class GlobalService : IGlobalService
 {
+    private readonly IUserPreferenceService userPrefService;
+    private readonly IUserIdeaInteractionService uiiService;
+
     public UserDto CurrentUser { get; set; }
+    private Dictionary<Guid, UserPreference> CurrentUserPreferences { get; set; }
+    private bool PreferencesPopulated { get; set; } = false;
+    public GlobalService(
+        IUserPreferenceService userPrefService,
+        IUserIdeaInteractionService uiiService)
+    {
+        this.userPrefService = userPrefService;
+        this.uiiService = uiiService;
+    }
+
+    public async Task<Dictionary<Guid, UserPreference>> GetPreferencesAsync()
+    {
+        if (!PreferencesPopulated)
+        {
+            var userPreferencesList = await userPrefService
+                .GetPreferencesOfUserAsync(CurrentUser.Username);
+            PreferencesPopulated = true;
+            return userPreferencesList.ToDictionary(up => up.ItemId);
+        }
+        return CurrentUserPreferences;
+    }
 }
