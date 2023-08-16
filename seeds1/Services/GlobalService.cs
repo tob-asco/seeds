361dfc2b98b6ff6fc31d72deabbfcec877834616
tob-asco <tobias.hochreiter@gmx.at>
2023-08-16 15:32:44 +0200

    GetIdeaInteractionsOfIdea: consumed in GlobalService

--- seeds1/Services/GlobalService.cs
+++ seeds1/Services/GlobalService.cs
@@ -13,6 +13,8 @@ public class GlobalService : IGlobalService
     public UserDto CurrentUser { get; set; }
     private Dictionary<Guid, UserPreference> CurrentUserPreferences { get; set; }
     private bool PreferencesPopulated { get; set; } = false;
+    private Dictionary<int, UserIdeaInteraction> CurrentUserIdeaInteractions { get; set; }
+    private bool IdeaInteractionsPopulated { get; set; } = false;
     public GlobalService(
         IUserPreferenceService userPrefService,
         IUserIdeaInteractionService uiiService)
@@ -21,6 +23,10 @@ public class GlobalService : IGlobalService
         this.uiiService = uiiService;
     }
 
+    /// <summary>
+    /// Gets all UserPrefernces of the CurrentUser
+    /// </summary>
+    /// <returns>A dictionary with the key given by the ItemId</returns>
     public async Task<Dictionary<Guid, UserPreference>> GetPreferencesAsync()
     {
         if (!PreferencesPopulated)
@@ -31,5 +37,21 @@ public class GlobalService : IGlobalService
             return userPreferencesList.ToDictionary(up => up.ItemId);
         }
         return CurrentUserPreferences;
+    }
+
+    /// <summary>
+    /// Gets all UserIdeaInteractions of the CurrentUser
+    /// </summary>
+    /// <returns>A dictionary with the key given by the IdeaId</returns>
+    public async Task<Dictionary<int, UserIdeaInteraction>> GetIdeaInteractionsAsync()
+    {
+        if (!IdeaInteractionsPopulated)
+        {
+            var ideaInteractionsList = await uiiService
+                .GetIdeaInteractionsOfUserAsync(CurrentUser.Username);
+            IdeaInteractionsPopulated = true;
+            return ideaInteractionsList.ToDictionary(uii => uii.IdeaId);
+        }
+        return CurrentUserIdeaInteractions;
     }
 }
