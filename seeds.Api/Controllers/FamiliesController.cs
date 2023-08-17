c1956971212c80d8562f3047a805c1e3423d0091
tob-asco <tobias.hochreiter@gmx.at>
2023-08-17 22:48:07 +0200

    FamiliesController: removed all but GetAll endpoint and tested it

--- seeds.Api/Controllers/FamiliesController.cs
+++ seeds.Api/Controllers/FamiliesController.cs
@@ -18,97 +18,15 @@ namespace seeds.Api.Controllers
 
         // GET: api/Families
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Family>>> GetFamily()
-        {
-          if (_context.Family == null)
-          {
-              return NotFound();
-          }
-            return await _context.Family.ToListAsync();
-        }
-
-        // GET: api/Families/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Family>> GetFamily(Guid id)
-        {
-          if (_context.Family == null)
-          {
-              return NotFound();
-          }
-            var family = await _context.Family.FindAsync(id);
-
-            if (family == null)
-            {
-                return NotFound();
-            }
-
-            return family;
-        }
-
-        // PUT: api/Families/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutFamily(Guid id, Family family)
-        {
-            if (id != family.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(family).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!FamilyExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Families
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Family>> PostFamily(Family family)
-        {
-          if (_context.Family == null)
-          {
-              return Problem("Entity set 'seedsApiContext.Family'  is null.");
-          }
-            _context.Family.Add(family);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetFamily", new { id = family.Id }, family);
-        }
-
-        // DELETE: api/Families/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteFamily(Guid id)
+        public async Task<ActionResult<IEnumerable<Family>>> GetFamilies()
         {
             if (_context.Family == null)
             {
                 return NotFound();
             }
-            var family = await _context.Family.FindAsync(id);
-            if (family == null)
-            {
-                return NotFound();
-            }
-
-            _context.Family.Remove(family);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            var fams = await _context.Family.ToListAsync();
+            if(fams == null || fams.Count == 0) { return NotFound(); }
+            return fams;
         }
 
         private bool FamilyExists(Guid id)
