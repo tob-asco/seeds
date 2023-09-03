c9ee149f532159828e02f27cce6c43cafd421bd6
tob-asco <tobias.hochreiter@gmx.at>
2023-09-03 12:08:24 +0200

    removed IdeaFromDb.CategoryKey

--- seeds.Dal/Dto/FromDb/IdeaFromDb.cs
+++ seeds.Dal/Dto/FromDb/IdeaFromDb.cs
@@ -7,7 +7,6 @@ public class IdeaFromDb
     public string Slogan { get; set; } = "Idea's Short Slogan";
     public string CreatorName { get; set; } = "tobi";
     public DateTime CreationTime { get; set; } = DateTime.Now;
-    public string CategoryKey { get; set; } = "NoC";
     public string Slide1 { get; set; } = "https://images.twinkl.co.uk/tr/raw/upload/u/ux/lightbulb-1875247-1920_ver_1.jpg";
     public string Slide2 { get; set; } = "";
     public string Slide3 { get; set; } = "";
