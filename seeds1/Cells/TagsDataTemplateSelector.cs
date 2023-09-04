974a1078361aac0149de756952c50af7e3a37820
tob-asco <tobias.hochreiter@gmx.at>
2023-09-04 19:23:30 +0200

    add selector and consume in view

---
+++ seeds1/Cells/TagsDataTemplateSelector.cs
@@ -0,0 +1,15 @@
+﻿using seeds1.MauiModels;
+
+namespace seeds1.Cells;
+
+public class TagsDataTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate FamilyTemplate { get; set; }
+    public DataTemplate PreferenceTemplate { get; set; }
+    protected override DataTemplate OnSelectTemplate(
+        object item, BindableObject container)
+    {
+        var fop = (FamilyOrPreference)item;
+        return fop.IsFamily ? FamilyTemplate : PreferenceTemplate;
+    }
+}
